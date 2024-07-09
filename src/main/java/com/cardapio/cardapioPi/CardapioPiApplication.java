b1426f6fd76127cdd265612f0b206efcedd7dec9
Gabriel Viana <gabvc2011@outlook.com>
2024-07-09 16:40:56 -0300

    Adicionando telaCadCliente

---
+++ src/main/java/com/cardapio/cardapioPi/CardapioPiApplication.java
@@ -0,0 +1,13 @@
+package com.cardapio.cardapioPi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapioPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapioPiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cardapio/cardapioPi/CardapioPiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardapio.cardapioPi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
